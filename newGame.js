288cc09180036909c37a3071ae9ad38db400c5f7
Wagnerzachef <112979117+Wagnerzachef@users.noreply.github.com>
2025-03-27 09:35:04 -0400

    Added Stuff

--- newGame.js
+++ newGame.js
@@ -4,6 +4,10 @@ const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
+const img1 = document.getElementById('quizImg1');
+const img2 = document.getElementById('quizImg2');
+const img3 = document.getElementById('quizImg3');
+const img4 = document.getElementById('quizImg4');
 
 let currentQuestion = {}
 let acceptingAnswers = true
@@ -13,15 +17,135 @@ let availableQuestions = []
 
 let questions = [
     {
-        question: 'What is this art piece ?',
+        question: 'What is the name of this art piece?',
         imgSrc: 'images/impression.jpg',
         choice1: "San Giorgio Maggiore at Dusk by Claud Monet",
+        image1: '',
         choice2: 'Impression, Sunrise by Claude Monet',
+        image2: '',
         choice3: 'The Houses of Parliment, Sunset by Claude Monet',
+        image3: '',
         choice4: 'Reflections of Clouds on the Water-Lily Pond by Claude Monet',
+        image4: '',
         answer: 2
+    },
+    {
+        question: 'Which one of these was made by Vincent Van Gogh?',
+        imgSrc: '',
+        choice1: '',
+        image1: 'images/star.jpg',
+        choice2: '',
+        image2: 'images/whitestar.jpg',
+        choice3: '',
+        image3: 'images/starry.jpg',
+        choice4: '',
+        image4: 'images/fakestar.jpg',
+        answer: 1
+    },
+    {
+        question: 'Who made this painting?',
+        imgSrc: 'images/thompson.jpg',
+        choice1: 'Donna Young',
+        image1: '',
+        choice2: 'Caroline Zimmermann',
+        image2: '',
+        choice3: 'Amy Everhart',
+        image3: '',
+        choice4: 'Elizabeth Thompson',
+        image4: '',
+        answer: 4
+    },
+    {
+        question: 'Who made this painting?',
+        imgSrc: 'images/scream.jpg',
+        choice1: 'Salvador Dali',
+        image1: '',
+        choice2: 'Vincent Van Gogh',
+        image2: '',
+        choice3: 'Edvard Munch',
+        image3: '',
+        choice4: 'Eugene Delacroix',
+        image4: '',
+        answer: 3
+    },
+    {
+        question: 'Which painting was made by Katsushika Hokusai?',
+        imgSrc: '',
+        choice1: '',
+        image1: 'images/whitestar.jpg',
+        choice2: '',
+        image2: 'images/great.jpg',
+        choice3: '',
+        image3: 'images/spring.jpg',
+        choice4: '',
+        image4: 'images/birdplum.jpg',
+        answer: 2
+    },
+    {
+        question: 'When was Andy Warhol born?',
+        imgSrc: '',
+        choice1: '1928',
+        image1: '',
+        choice2: '1912',
+        image2: '',
+        choice3: '1898',
+        image3: '',
+        choice4: '1920',
+        image4: '',
+        answer: 1
+    },
+    {
+        question: 'Who directed the movie shown in the picture?',
+        imgSrc: 'images/lotr.jpg',
+        choice1: 'Steven Speilberg',
+        image1: '',
+        choice2: 'Peter Jackson',
+        image2: '',
+        choice3: 'Woody Allen',
+        image3: '',
+        choice4: 'Robert Altman',
+        image4: '',
+        answer: 3
+    },
+    {
+        question: 'Which painting made by Leonardo Da Vinci is permanently displayed in the Louvvre Museum?',
+        imgSrc: '',
+        choice1: 'Salvator Mundi',
+        image1: '',
+        choice2: 'The Lady with an Ermine',
+        image2: '',
+        choice3: 'The Last Supper',
+        image3: '',
+        choice4: 'Mona Lisa',
+        image4: '',
+        answer: 4
+    },
+    {
+        question: 'Which of these paintings by Johannes Vermeer is currently missing?',
+        imgSrc: '',
+        choice1: 'The Astronomer',
+        image1: 'images/astronomer.jpg',
+        choice2: 'The Concert',
+        image2: 'images/concert.jpg',
+        choice3: 'The Art of Painting',
+        image3: 'images/painting.jpg',
+        choice4: 'The Milkmaid',
+        image4: 'images/milk.jpg',
+        answer: 2
+    },
+    {
+        question: 'The largest art theft in the world took place in what year?',
+        imgSrc: '',
+        choice1: '1990',
+        image1: '',
+        choice2: '1940',
+        image2: '',
+        choice3: '2010',
+        image3: '',
+        choice4: '1999',
+        image4: '',
+        answer: 1
     }
-    
 ]
 
 const SCORE_POINTS = 100
@@ -50,6 +174,13 @@ getNewQuestion = () => {
     question.innerText = currentQuestion.question
 
     quizImg.innerHTML = '<img src=' + currentQuestion.imgSrc + '>'
+
+    img1.innerHTML = '<img src=' + currentQuestion.image1 + '>'
+    img2.innerHTML = '<img src=' + currentQuestion.image2 + '>'
+    img3.innerHTML = '<img src=' + currentQuestion.image3 + '>'
+    img4.innerHTML = '<img src=' + currentQuestion.image4 + '>'
+
+    
 
     choices.forEach(choice => {
         const number = choice.dataset['number']
